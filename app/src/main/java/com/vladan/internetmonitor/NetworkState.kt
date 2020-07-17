866f9bb9afc27eb60a548facd6587ec90d39b128
vladan <vladan.starcevic@gmail.com>
2020-07-17 22:12:34 +0200

    Add NetworkState.kt

---
+++ app/src/main/java/com/vladan/internetmonitor/NetworkState.kt
@@ -0,0 +1,10 @@
+package com.vladan.internetmonitor
+
+/**
+ * Created by vladan on 7/17/2020
+ */
+data class NetworkState(
+    var isConnected: Boolean,
+    var connectionType: Int,
+    var networkSubtype: Int
+)
